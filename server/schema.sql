61e49b1c2fc66ce75fd67daa3af7c7a05ef01b31
dorktobes <t.hoshman@gmail.com>
2017-11-03 16:36:35 -0700

    set up db, about to build out server

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,32 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms(
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  room_name text
+);
+
+CREATE TABLE users(
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  user_name text
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  message text,
+  room integer,
+  user integer,
+  CONSTRAINT
+  FOREIGN KEY (room) REFERENCES rooms(id),
+  CONSTRAINT
+  FOREIGN KEY (user) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
+
 
 
 
