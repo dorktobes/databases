fb301da913f8423222f14e359e42c59f53f35262
dorktobes <t.hoshman@gmail.com>
2017-11-03 19:05:43 -0700

    rendering index.html from server reqeuest

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,13 @@
 var models = require('../models');
 
 module.exports = {
+  
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      // models.messages.get(data => {
+        
+      // });
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,36 @@
 var db = require('../db');
+//var connection = require('../db/index.js');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      // db.connection.query('SELECT * FROM messages', function(err, results) {
+      //   if (err) {
+      //     throw err;
+      //   } else {
+      //     console.log('results:', results);
+      //   }
+      // });
+      //query all messages from database
+        //messages will be deconstruced
+        /*
+        {messageId: 1,
+        message: 'hello world',
+        user: 5 
+        room: 2},
+        */
+    }, // a function which produces all the messages
+    post: function () {
+      
+      /*
+      { message: 'hello world',
+        user: 'fred'--> 5, 
+        room: 'lobby' --> 2}
+      --insert room into rooms table, retrieve room id,
+      --insert user into users table, retrieve user id,
+      --insert message into messages table with user and room id's
+      */
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
@@ -13,3 +40,4 @@ module.exports = {
   }
 };
 
+//y u no room
